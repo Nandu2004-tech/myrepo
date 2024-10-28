208f5c5bb261913d2f44f1f7d0c419cbfdc2a984
unknown <knandunayak@gmail.com>
2024-10-29 00:01:48 +0530

    added confim in js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Thank you for your message, ' + document.getElementById('name').value + '!');
+    confirm("can i fuck u")
     this.reset(); // Reset the form fields
 });
